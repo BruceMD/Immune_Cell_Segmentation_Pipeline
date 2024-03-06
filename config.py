bb659faa8a7b34f8f1f7e84080659897153202e3
BruceMD <maxbruce9@gmail.com>
2024-03-06 10:38:31 +0000

    Missed bin/ directory in OpenSlide

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 PROJECT_DIRECTORY = "D:/Bruce_Data/QuPathProject/project.qpproj"
-OPENSLIDE_DIRECTORY = "openslide-win64-20231011"
+OPENSLIDE_DIRECTORY = r"C:/Users/c92568bd/Documents/Immune_Cell_Segementation_Pipeline/openslide-win64-20231011/bin"
 SLIDE_PATH = "D:/Bruce_Data/02_human_bd/01_mrxs"
