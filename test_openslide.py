14e7fb0d1a63c09fb028ab50103dbaff1bf6c37d
BruceMD <maxbruce9@gmail.com>
2024-03-07 12:09:52 +0000

    New testing

--- test_openslide.py
+++ test_openslide.py
@@ -1,41 +1,41 @@
+from config import PROJECT_DIRECTORY
+from pathlib import Path
+from paquo.projects import QuPathProject
+
 import os
-OPENSLIDE_PATH = r'C:/Users/maxbr/PycharmProjects/HistologyRP/openslide-win64-20231011/bin'
+from config import OPENSLIDE_DIRECTORY, SLIDE_PATH
 if hasattr(os, 'add_dll_directory'):
-    # Windows
-    with os.add_dll_directory(OPENSLIDE_PATH):
+    with os.add_dll_directory(OPENSLIDE_DIRECTORY):
         import openslide
 else:
     import openslide
 
-
-def test():
-
-    level = 0
-    slide = openslide.OpenSlide('C:/Users/maxbr/Documents/University/BIOL61230'
-                                ' - Research Project 1/Example data/R3-S21-2021-11-18T20-41-45.mrxs')
-    output = 'test_data'
-
-    width, height = slide.dimensions
-    print(f'Width: {width}')
-    print(f'Height: {height}')
-
-    count = 0
-    for y in range(0, height, 2048):
-        for x in range(0, width, 2048):
-            count += 1
-            # tile = slide.read_region((x, y), 0, (2048, 2048))
-            # if black_tile(tile) or white_tile(tile):
-            #     pass
-            # else:
-            #     tile.convert('RGB').save(f'test_data/test_tile_{count}_{x}_{y}.png')
-    print(count)
+from OpenSlideExportTiles import black_tile
 
 
-def white_tile(image, threshold=230):
-    avg_color = image.resize((1, 1)).getpixel((0, 0))  # Get average by resizing
-    return all(channel >= threshold for channel in avg_color)
+def orchestrate():
+    """
+    Facilitate workflow:
+        1. Open QuPath project
+        2. Iterate through images loaded in project
+        3. Iterate through annotations in images - add 512X512 tiles as keys to dic
+        4. Iterate through annotations again - add marked cells to value list if co-ords in tile
+        5. Pass image name and dictionary to distribute_tiles
+    """
+
+    slide = openslide.OpenSlide(f'D:/Bruce_Data/02_human_bd/01_mrxs/R5-S14-2021-11-18T22-47-44.mrxs')
+
+    print(slide.dimensions)
+
+    for i in range(220):
+        for j in range(220):
+
+            tile = slide.read_region((4000*i, 3000*j), 0, (4000, 3000))
+
+            if black_tile(tile):
+                continue
+
+            tile.convert('RGB').save(format='TIFF', fp=f'fuck/temp_image_{i}_{j}.tiff')
 
 
-def black_tile(image, threshold=25):
-    avg_color = image.resize((1, 1)).getpixel((0, 0))  # Get average by resizing
-    return all(channel <= threshold for channel in avg_color)
+orchestrate()
