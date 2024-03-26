820c0a870cce87846e8b3f7cb4ec49515140cb61
BruceMD <maxbruce9@gmail.com>
2024-03-26 14:58:27 +0000

    feat: update directory structure

--- config.py
+++ config.py
@@ -2,3 +2,6 @@ PROJECT_DIRECTORY = "D:/Bruce_Data/QuPathProject/project.qpproj"
 OPENSLIDE_DIRECTORY = r"C:/Users/c92568bd/Documents/Immune_Cell_Segmentation_Pipeline/openslide-win64-20231011/bin"
 SLIDE_PATH = "D:/Bruce_Data/02_human_bd/01_mrxs"
 QUPATH_ROTATED = True
+
+DATA_OUTPUT_PATH = "data/data"
+MASKS_OUTPUT_PATH = "data/data_masks"
